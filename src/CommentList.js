5439df5b81c062f237bf12161b4cfa623f5e9e9d
ipc103 <iancandysemail@gmail.com>
2017-04-28 13:18:20 -0400

    add comment list

---
+++ src/CommentList.js
@@ -0,0 +1,14 @@
+class CommentList {
+  constructor(){
+    this.comments = []
+  }
+
+  render(){
+    return `<ul>${this.comments.map(comment => comment.render() ).join('')}</ul>`
+  }
+
+  addComment(text){
+    const comment = new Comment(text)
+    this.comments.push(comment)
+  }
+}
