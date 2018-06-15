2c7e18773245141f4b969b2d10e35a1549d04181
Nicole D <dalcin.nicole@gmail.com>
2018-06-15 09:59:57 -0400

    update urls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
+
   const imageId = 1 //Enter your assigned imageId here
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-  const likeURL = `https://randopic.herokuapp.com/likes/`
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+  const imageURL = `https://randopic-challenge.herokuapp.com/images/${imageId}`
+
+  const likeURL = `https://randopic-challenge.herokuapp.com/likes/`
+
+  const commentsURL = `https://randopic-challenge.herokuapp.com/comments/`
+
+
 
 })
+
