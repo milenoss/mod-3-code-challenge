91030f98f7d6f416fede3114cc65002bcde61f11
Andrew <19658749+realAndrewCohn@users.noreply.github.com>
2018-09-27 17:16:10 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
   const yourUUID = 1 //Enter your assigned uuid here
 
   const imageURL = `https://randopic.herokuapp.com/images/${yourUUID}`
+  
+  const imageId = 1 //Enter the id from the fetched image here
 
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
